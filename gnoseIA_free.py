e7d23ac68b28d6e796d28f34321bd7b07f7edde1
boniface <sitraka.boniface@gmail.com>
2024-06-25 09:13:55 +0300

    update code

--- gnoseIA_free.py
+++ gnoseIA_free.py
@@ -136,7 +136,6 @@ def initialize_tokenizer(model_name: str):
     :param model_name: Name or path of the model for tokenizer initialization.
     :return: Initialized tokenizer.
     """
-    #tokenizer = AutoTokenizer.from_pretrained(model_name)
     tokenizer = AutoTokenizer.from_pretrained(model_name)
     tokenizer.bos_token_id = 1  # Set beginning of sentence token id
     return tokenizer
